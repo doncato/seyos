a87a99d4d62d5f967184a64554ce454f6f89d29f
doncato <don.cato.dc11@gmail.com>
2021-10-16 00:31:43 +0200

    Updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,58 @@
 // By doncato.
 
 // Imports
-
 use chrono::Utc;
 use discord_rpc_client::Client as RPC;
 use log::*;
-use std::{convert::TryFrom, thread, time::Duration};
+use std::{
+    convert::TryFrom,
+    thread,
+    time::{Duration, SystemTime},
+};
 use sysinfo::{System, SystemExt};
 
+// Main function
 fn main() {
+    info!("Started");
     // Set the System Instance
     let mut sys = System::new_all();
     // Set the Discord Client instance
     let mut rpc = RPC::new(898584015076982865);
-    let refresh_interval = Duration::from_secs(20);
+    let refresh_time = 20;
+    let refresh_interval = Duration::from_secs(refresh_time);
 
     // Start the Client instance
     rpc.start();
+    debug!("Started the Discord Client Instance");
+
+    // Presence Information
+    let mut infos = PresenceInfo::empty();
 
     // Start the main loop
     loop {
-        sys.refresh_all();
+        let start = SystemTime::now();
 
-        println!("{:?}", get_os(&sys));
+        debug!("Started the main event loop");
+        sys.refresh_system();
+        let os = get_os(&sys);
+        infos.os_name = sys.long_os_version().unwrap_or(os.0);
+        infos.asset_name = os.1;
+        infos.uptime = sys.uptime();
+        infos.information = format!("Load: {}", sys.load_average().five);
 
-        thread::sleep(refresh_interval);
+        let set = infos.set(rpc);
+        rpc = set.0;
+        if !set.1 {
+            error!(
+                "Couldn't set the Discord Rich Presence! Retrying in about {} seconds",
+                refresh_time
+            )
+        }
+
+        thread::sleep(
+            refresh_interval
+                - (SystemTime::now().duration_since(start)).unwrap_or(Duration::from_secs(0)),
+        );
     }
 }
 
@@ -51,23 +79,15 @@ struct PresenceInfo {
     os_name: String,
     information: String,
     asset_name: String,
-    asset_text: String,
-    uptime: u32,
+    uptime: u64,
 }
 impl PresenceInfo {
     /// Create a new Presence Info
-    fn new(
-        os_name: String,
-        information: String,
-        asset_name: String,
-        asset_text: String,
-        uptime: u32,
-    ) -> Self {
+    fn new(os_name: String, information: String, asset_name: String, uptime: u64) -> Self {
         PresenceInfo {
             os_name,
             information,
             asset_name,
-            asset_text,
             uptime,
         }
     }
@@ -77,20 +97,22 @@ impl PresenceInfo {
             os_name: "linux".to_string(),
             information: "".to_string(),
             asset_name: "default".to_string(),
-            asset_text: "linux".to_string(),
-            uptime: u32::try_from(Utc::now().timestamp()).expect("Time went backwards"),
+            uptime: u64::try_from(Utc::now().timestamp()).expect("Time went backwards."),
         }
     }
 
     /// Set the Presence Info
-    fn set(self, mut discord_rpc: RPC) -> Result<(), ()> {
+    fn set(&self, mut discord_rpc: RPC) -> (RPC, bool) {
         match discord_rpc.set_activity(|a| {
             a.state(&self.information)
                 .details(&self.os_name)
-                .assets(|ass| ass.large_image(self.asset_name).large_text(self.asset_text))
+                .assets(|ass| {
+                    ass.large_image(&self.asset_name)
+                        .large_text(&self.asset_name)
+                })
         }) {
-            Ok(_) => Ok(()),
-            Err(_) => Err(()),
+            Ok(_) => (discord_rpc, true),
+            Err(_) => (discord_rpc, false),
         }
     }
 }
